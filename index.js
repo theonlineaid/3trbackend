667bdcb08b5126565bf1a7c1cfcdf1cce2604f3d
theonlineaid <nkomorfaruk@gmail.com>
2026-08-06 15:09:25 +0600

    Add prod compose

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const { Pool } = pg;
 
 const app = express();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? { connectionString: process.env.DATABASE_URL }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      }
+);
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello Docker 🚀" });
